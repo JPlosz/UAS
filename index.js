d4acc91f2e5120b729ee65e8bb9b8f3c8fafb205
Joshua Plosz <joshua.plosz@gmail.com>
2021-12-18 06:50:02 -0700

    template ejs and working home page

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const app = express()
 
-let rqst = 0;
-app.use(() => {
-    rqst++;
-    console.log("=>HTML REQUEST " + rqst + "<=")
+// let rqst = 0;
+// app.use(() => {
+//     rqst++;
+//     console.log("=>HTML REQUEST " + rqst + "<=")
+// })
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => {
+    res.render("home.ejs")
 })
-
-
 
 let port = 50505;
 app.listen(port, () => {
